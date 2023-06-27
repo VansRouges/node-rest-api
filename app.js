1b6d074b10fac1c93dba8395866f674fac7dc826
VansRouges <evansagina57@gmail.com>
2023-06-28 00:25:29 +0100

    installed cors and morgan

--- app.js
+++ app.js
@@ -1,19 +1,53 @@
-const express = require('express');
-// require('dotenv').config();
+const Express = require("express");
+const app = Express();
+const cors = require("cors");
+const morgan = require("morgan");
+const { Sequelize } = require("sequelize");
 
-const app = express();
-app.use(express.json());
+const { port } = require("./config");
+const PORT = process.env.PORT || port;
 
-const PORT = process.env.PORT || 3000;
+// Express Routes Import
+const AuthorizationRoutes = require("./authorization/routes");
+const UserRoutes = require("./users/routes");
+const ProductRoutes = require("./products/routes");
 
-app.listen(PORT, () => {
-    console.log("Server Listening on PORT:", PORT);
+// Sequelize model imports
+const UserModel = require("./common/models/User");
+const ProductModel = require("./common/models/Product");
+
+app.use(morgan("tiny"));
+app.use(cors());
+
+// Middleware that parses the body payloads as JSON to be consumed next set
+// of middlewares and controllers.
+app.use(Express.json());
+
+const sequelize = new Sequelize({
+  dialect: "sqlite",
+  storage: "./storage/data.db", // Path to the file that will store the SQLite DB.
 });
 
-app.get('/status', (request, response) => {
-    const status = {
-        "status": "Running"
-    };
+// Initialising the Model on sequelize
+UserModel.initialise(sequelize);
+ProductModel.initialise(sequelize);
 
-    response.send(status);
-});
+// Syncing the models that are defined on sequelize with the tables that alredy exists
+// in the database. It creates models as tables that do not exist in the DB.
+sequelize
+  .sync()
+  .then(() => {
+    console.log("Sequelize Initialised!!");
+
+    // Attaching the Authentication and User Routes to the app.
+    app.use("/", AuthorizationRoutes);
+    app.use("/user", UserRoutes);
+    app.use("/product", ProductRoutes);
+
+    app.listen(PORT, () => {
+      console.log("Server Listening on PORT:", port);
+    });
+  })
+  .catch((err) => {
+    console.error("Sequelize Initialisation threw an error:", err);
+  });
